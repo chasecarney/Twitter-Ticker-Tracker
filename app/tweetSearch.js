373dc549891eb60bef163454d3a7e4d2b05e3ce5
Chase Carney <jcca247@uky.edu>
2017-04-15 22:35:31 -0400

    tweetSearch implement

--- app/tweetSearch.js
+++ app/tweetSearch.js
@@ -1,16 +1,26 @@
 //call back functions
 var error = function (err, response, body) {
-	console.log('ERROR [%s]', err);
+	console.log('ERROR', err);
 };
 	var success = function (data) {
-	console.log('Data [%s]', data);
+	data = JSON.parse(data);
+	for(var x in data.statuses)
+		console.log(data.statuses[x].text);
 };
 
 var Twitter = require('twitter-node-client').Twitter;
 
-var twitter = new Twitter();
+var config = {
+    	"consumerKey": "hbOU1pfUSWt3jXKJUu5Y5pMMT",
+    	"consumerSecret": "eOO1LCmJdEuSMySlP2mewrhwX6xUvHgLNfH8tcwIy8EVDNEof5",
+    	"accessToken": "714958449323454468-VOEhP0EQrFDS9hC1STBeMPrb3cALSDQ",
+    	"accessTokenSecret": "urZnQ5jTcPMTWxPZ9IzWVqPIxM8OglvKrRc67TUFcFkec",
+    	"callBackUrl": ""
+	};
+
+var twitter = new Twitter(config);
 
 function search()
 {
-	console.log(twitter.getSearch({'q':'$JNUG','result\_type':'popular'},error,success));
+	twitter.getSearch({'q':'$JNUG'}, error, success);
 }
